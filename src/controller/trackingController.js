a0d8471d9c0efb2e586e4b549c7d38bee991e695
Andres F. Barco <andrez.fbs@gmail.com>
2021-01-14 10:55:51 -0500

    Just testing real time connection... not yet

--- src/controller/trackingController.js
+++ src/controller/trackingController.js
@@ -1,56 +1,71 @@
-
-import { query } from 'express';
-import { model } from 'mongoose';
-import models from '../models';
+import { query } from "express";
+import { model } from "mongoose";
+import models from "../models";
+// import { io } from "socket.io";
+// import async from "async";
+// var io = require('socket.io')(http);
 
 /**
  * POST - GET request.
  */
+
+/* var socketMap = [];
+io.on("connection", (socket) => {
+  console.log("Client Connected");
+  socketMap.push(socket);
+  dataUpdate();
+}); */
+
 export default {
-    add: async (req, res, next) => {
-        try {
-            const reg = await models.Tracking.create(req.body);
-            res.status(200).json(reg);
-        } catch (error) {
-            res.status(500).send({ message: 'ERROR' });
-            next(error);
-        }
-    },
-    list: async (req, res, next) => {
-        try {
-            const reg = await models.Tracking.find({});      
-            res.status(200).json(reg);
-        } catch (error) {
-            res.status(500).send({ message: 'ERROR' });
-            next(error);
-        }
-    },
-    deleteall: async(req, res, next)=>{
-        try {
-            const reg = await models.Tracking.deleteMany({})
-            res.send({message: 'Base de Datos limpia'})
-            //res.status(200);
-        } catch (error) {
-            res.status(500).send({ message: 'ERROR' });
-            next(error);
-        }
-    },
-    init: async(req,res,next) =>{
-        try {
-            const reg = await models.Init.find({});
-            res.status(200).json(reg);
-        } catch (error) {
-            res.status(500).send({ message: 'ERROR' });
-            next(error);
-        }
-    },
-    addinit: async(req,res,next) =>{
-        try {
-            const reg = await models.Init.create(req.body);
-            res.status(200).json(reg);
-        } catch (error) {
-            res.status(500).send({ message: 'ERROR' });
-            next(error);
-        }
+  add: async (req, res, next) => {
+    try {
+      const reg = await models.Tracking.create(req.body);
+      // dataUpdate();
+      res.status(200).json(reg);
+    } catch (error) {
+      res.status(500).send({ message: "ERROR" });
+      next(error);
     }
-}
+  },
+  list: async (req, res, next) => {
+    try {
+      const reg = await models.Tracking.find({});
+      res.status(200).json(reg);
+    } catch (error) {
+      res.status(500).send({ message: "ERROR" });
+      next(error);
+    }
+  },
+  deleteall: async (req, res, next) => {
+    try {
+      const reg = await models.Tracking.deleteMany({});
+      res.send({ message: "Base de Datos limpia" });
+      //res.status(200);
+    } catch (error) {
+      res.status(500).send({ message: "ERROR" });
+      next(error);
+    }
+  },
+  init: async (req, res, next) => {
+    try {
+      const reg = await models.Init.find({});
+      res.status(200).json(reg);
+    } catch (error) {
+      res.status(500).send({ message: "ERROR" });
+      next(error);
+    }
+  },
+  addinit: async (req, res, next) => {
+    try {
+      const reg = await models.Init.create(req.body);
+      res.status(200).json(reg);
+    } catch (error) {
+      res.status(500).send({ message: "ERROR" });
+      next(error);
+    }
+  },
+};
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,37 +1,66 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import mongoose, { mongo } from 'mongoose';
-import path from 'path';
-import router from './routes';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose, { mongo } from "mongoose";
+import path from "path";
+import router from "./routes";
+import models from "./models";
+// import http  from 'http';
+// import { io } from "socket.io";
+// import async from "async";
 
 mongoose.Promise = global.Promise;
 
 /** Conexion Base de Datos Mongo DB Atlas. */
-const dbUrl = 'mongodb+srv://tracking:m0n1t0r30@trackingapi.pk5kg.mongodb.net/Tracking?retryWrites=true&w=majority';
+const dbUrl =
+  "mongodb+srv://tracking:m0n1t0r30@trackingapi.pk5kg.mongodb.net/Tracking?retryWrites=true&w=majority";
 
-mongoose.connect(dbUrl, {useNewUrlParser: true,useUnifiedTopology:true})
-.then(mongoose => console.log('Conectando a la base de datos'))
-.catch(err => console.log(err));
+mongoose
+  .connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((mongoose) => console.log("Conectando a la base de datos"))
+  .catch((err) => console.log(err));
 
+const app = express("express");
+const http = require("http").createServer(app);
 
-const app = express();
-app.use(morgan('dev'));
-app.use(cors());
-
-
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
-app.use(express.static( path.join(__dirname + '\\public')));
-
-app.use('/api',router);
-
-app.set('port', process.env.PORT || 3000);
-
-
-//Gestion de las Rutas
-app.listen(app.get('port'),()=>{
-    console.log('Server on port ' + app.get('port'));
+var io = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST"],
+  },
 });
 
+var socketMap = [];
+io.on("connection", (socket) => {
+  console.log("Client Connected");
+  socketMap.push(socket);
+  dataUpdate();
+});
+
+app.use(morgan("dev"));
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname + "\\public")));
+
+app.use("/api", router);
+
+app.set("port", process.env.PORT || 3000);
+
+//Gestion de las Rutas
+http.listen(3000, "localhost");
+/* app.listen(app.get("port"), () => {
+  console.log("Server on port " + app.get("port"));
+}); */
+
+async function dataUpdate() {
+  console.log("Socket Emmit");
+  var posiciones = await models.Tracking.find({});
+  for (let socketMapObj of socketMap) {
+    if (posiciones.length > 0) {
+      socketMapObj.emit("dataUpdate", posiciones);
+    }
+  }
+}
